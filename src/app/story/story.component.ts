befdd060a2f8c129560fa3095e6e9e10d9dd75c9
Stefan Ugrinov <sugrinov@execom.eu>
2018-04-23 17:24:23 +0200

    Add pages component and story component

---
+++ src/app/story/story.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../item.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.css']
+})
+export class StoryComponent implements OnInit {
+
+  storyID: number;
+  story: Object;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.storyID = +this.route.snapshot.paramMap.get('id');
+  }
+
+}
