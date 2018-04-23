a07ad7cc0818b5303c43c7ada5c83427313e7f2c
Stefan Ugrinov <sugrinov@execom.eu>
2018-04-23 17:04:26 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MomentModule } from 'angular2-moment';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { TopStoriesComponent } from './top-stories/top-stories.component';
+import { TopStoriesService } from './top-stories.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ItemComponent } from './item/item.component';
+import { ItemService } from './item.service';
+import { CommonStoriesComponent } from './common-stories/common-stories.component';
+import { CommonStoriesService } from './common-stories.service';
+import { PagesComponent } from './pages/pages.component';
+import { CommentsComponent } from './comments/comments.component';
+import { CommentService } from './comment.service';
+import { StoryComponent } from './story/story.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { UserService } from './user.service';
+import { SubmissionsComponent } from './submissions/submissions.component';
+import { SubmissionsService } from './submissions.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopStoriesComponent,
+    ItemComponent,
+    CommonStoriesComponent,
+    PagesComponent,
+    CommentsComponent,
+    StoryComponent,
+    UserInfoComponent,
+    SubmissionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MomentModule
   ],
-  providers: [],
+  providers: [
+    TopStoriesService,
+    CommonStoriesService,
+    ItemService,
+    CommentService,
+    UserService,
+    SubmissionsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
